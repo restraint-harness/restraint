44a0d1da935fdd897ccda513563f65649eae4d03
Artem Savkov <asavkov@redhat.com>
2016-05-23 07:49:01 -0400

    curl: switch to multi mode.
    Using multi mode so that fetching is non-blocking. We still have to wait till
    we fetch all the data before starting the unpack process.
    
    Change-Id: Ib911576b2300766228ec246ba3856f193bc8ac60

--- src/fetch_http.c
+++ src/fetch_http.c
@@ -32,6 +32,17 @@
 #include "fetch.h"
 #include "fetch_http.h"
 
+struct curl_data {
+    CURLM *curlm;
+    int to_ev;
+    int running;
+};
+
+struct socket_data {
+    GIOChannel *ch;
+    int ev;
+};
+
 static size_t cwrite_callback(char *ptr, size_t size, size_t nmemb,
                               void *userdata)
 {
@@ -68,10 +79,12 @@ myopen(FetchData *fetch_data, GError **error)
 {
     g_return_val_if_fail(fetch_data != NULL, FALSE);
     g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
-    CURLcode res;
+    CURLMcode res;
     gchar ebuf[CURL_ERROR_SIZE];
-    CURL *curl = (CURL*)fetch_data->private_data;
+    struct curl_data *cd = fetch_data->private_data;
+    CURLM *curlm = cd->curlm;
     char *url = soup_uri_to_string(fetch_data->url, FALSE);
+    CURL *curl = curl_easy_init();
 
     fetch_data->istream = g_memory_input_stream_new();
     curl_easy_setopt(curl, CURLOPT_URL, url);
@@ -84,9 +97,9 @@ myopen(FetchData *fetch_data, GError **error)
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
     }
 
-    res = curl_easy_perform(curl);
+    res = curl_multi_add_handle(curlm, curl);
 
-    if (res != CURLE_OK) {
+    if (res != CURLM_OK) {
         g_set_error(error, RESTRAINT_FETCH_LIBARCHIVE_ERROR, 0,
                 "Failed to fetch url: %s", ebuf);
         g_free (url);
@@ -117,8 +130,9 @@ myclose(struct archive *a, void *client_data)
 static gboolean
 archive_finish_callback (gpointer user_data)
 {
-    FetchData *fetch_data = (FetchData *) user_data;
-    CURL *curl = (CURL*)fetch_data->private_data;
+    FetchData *fetch_data = (FetchData*)user_data;
+    struct curl_data *cd = fetch_data->private_data;
+    CURLM *curlm = cd->curlm;
     gint free_result;
 
     if (fetch_data == NULL) {
@@ -144,7 +158,8 @@ archive_finish_callback (gpointer user_data)
         g_clear_error(&fetch_data->error);
     }
 
-    curl_easy_cleanup(curl);
+    curl_multi_cleanup(curlm);
+    g_free(fetch_data->private_data);
     g_slice_free(FetchData, fetch_data);
     return FALSE;
 }
@@ -208,6 +223,142 @@ http_archive_read_callback (gpointer user_data)
     return TRUE;
 }
 
+static void check_multi_info(FetchData *fetch_data)
+{
+    struct curl_data *cd = fetch_data->private_data;
+    CURLM *curlm = cd->curlm;
+    CURLMsg *msg;
+    int msgs_left;
+
+    while((msg = curl_multi_info_read(curlm, &msgs_left))) {
+        if (msg->msg == CURLMSG_DONE) {
+            CURL *easy = msg->easy_handle;
+            curl_multi_remove_handle(curlm, easy);
+            curl_easy_cleanup(easy);
+        }
+    }
+}
+
+static gboolean event_cb(GIOChannel *ch, GIOCondition condition, gpointer data)
+{
+    CURLMcode res;
+    FetchData *fetch_data = (FetchData*)data;
+    struct curl_data *cd = fetch_data->private_data;
+    CURLM *curl = cd->curlm;
+    int fd = g_io_channel_unix_get_fd(ch);
+    int action = (condition & G_IO_IN ? CURL_CSELECT_IN : 0) |
+                 (condition & G_IO_OUT ? CURL_CSELECT_OUT : 0);
+
+    res = curl_multi_socket_action(curl, fd, action, &cd->running);
+    if (res != CURLM_OK) {
+        g_set_error(&fetch_data->error, RESTRAINT_FETCH_LIBARCHIVE_ERROR, res,
+                    "curl failed");
+        g_idle_add (archive_finish_callback, fetch_data);
+        return FALSE;
+    }
+
+    check_multi_info(fetch_data);
+    if (cd->running) {
+        return TRUE;
+    } else {
+        if (cd->to_ev != 0) {
+            g_source_remove(cd->to_ev);
+            cd->to_ev = 0;
+        }
+        return FALSE;
+    }
+}
+
+static int sock_cb(CURL *easy,      /* easy handle */
+                   curl_socket_t s, /* socket */
+                   int what,        /* describes the socket */
+                   void *userp,     /* private callback pointer */
+                   void *socketp)   /* private socket pointer */
+{
+    FetchData *fetch_data = (FetchData*)userp;
+    struct curl_data *cd = fetch_data->private_data;
+    CURLM *curl = cd->curlm;
+    struct socket_data *sd = socketp;
+    int action = (what & CURL_POLL_IN ? G_IO_IN : 0) |
+                 (what & CURL_POLL_OUT ? G_IO_OUT : 0);
+
+    if (what == CURL_POLL_REMOVE) {
+        if (sd == NULL) {
+            return 0;
+        }
+        if (sd->ev != 0) {
+            g_source_remove(sd->ev);
+            sd->ev = 0;
+        }
+        g_io_channel_unref(sd->ch);
+        g_free(sd);
+        curl_multi_assign(curl, s, NULL);
+    } else {
+        if (sd == NULL) {
+            sd = g_new0(struct socket_data, 1);
+            sd->ch = g_io_channel_unix_new(s);
+            curl_multi_assign(curl, s, sd);
+        }
+        if (sd->ev != 0) {
+            g_source_remove(sd->ev);
+            sd->ev = 0;
+        }
+        sd->ev = g_io_add_watch(sd->ch, action, event_cb, fetch_data);
+    }
+    return 0;
+}
+
+static gboolean timer_cb(gpointer data)
+{
+    FetchData *fetch_data = (FetchData*)data;
+    struct curl_data *cd = fetch_data->private_data;
+    CURLM *curl = cd->curlm;
+    CURLMcode res;
+    res = curl_multi_socket_action(curl, CURL_SOCKET_TIMEOUT, 0, &cd->running);
+    if (res != CURLM_OK) {
+        g_set_error(&fetch_data->error, RESTRAINT_FETCH_LIBARCHIVE_ERROR, res,
+                    "curl failed");
+        g_idle_add (archive_finish_callback, fetch_data);
+        return FALSE;
+    }
+    check_multi_info(fetch_data);
+    return FALSE;
+}
+
+static int update_timeout_cb(CURLM *multi,    /* multi handle */
+                             long timeout_ms, /* see above */
+                             void *userp)     /* private callback pointer */
+{
+    FetchData *fetch_data = (FetchData*)userp;
+    struct curl_data *cd = fetch_data->private_data;
+
+    if (timeout_ms >= 0) {
+        cd->to_ev = g_timeout_add(timeout_ms, timer_cb, userp);
+    }
+    return 0;
+}
+
+static gboolean start_unpack(gpointer data)
+{
+    FetchData *fetch_data = data;
+    struct curl_data *cd = fetch_data->private_data;
+
+    if (cd->running > 0) {
+        return TRUE;
+    }
+
+    gint r;
+    r = archive_read_open(fetch_data->a, fetch_data, NULL, myread, myclose);
+    if (r != ARCHIVE_OK) {
+        g_set_error(&fetch_data->error, RESTRAINT_FETCH_LIBARCHIVE_ERROR, r,
+                "archive_read_open failed: %s", archive_error_string(fetch_data->a));
+        g_idle_add (archive_finish_callback, fetch_data);
+        return FALSE;
+    }
+    g_idle_add (http_archive_read_callback, fetch_data);
+    return FALSE;
+}
+
 void
 restraint_fetch_http (SoupURI *url,
                      const gchar *base_path,
@@ -231,22 +382,10 @@ restraint_fetch_http (SoupURI *url,
     fetch_data->ssl_verify = ssl_verify;
 
     GError *tmp_error = NULL;
-    gint r;
 
     if (keepchanges == FALSE) {
         rmrf(base_path);
     }
-
-    CURL *curl = curl_easy_init();
-
-    if (curl == NULL) {
-        g_set_error(&fetch_data->error, RESTRAINT_FETCH_LIBARCHIVE_ERROR, 0,
-                "failed to init curl");
-        g_idle_add (archive_finish_callback, fetch_data);
-        return;
-    }
-
-    fetch_data->private_data = curl;
 
     fetch_data->a = archive_read_new();
     if (fetch_data->a == NULL) {
@@ -265,18 +404,30 @@ restraint_fetch_http (SoupURI *url,
     }
     archive_read_support_filter_all(fetch_data->a);
     archive_read_support_format_all(fetch_data->a);
+
+    struct curl_data *cd = g_new0(struct curl_data, 1);
+    cd->curlm = curl_multi_init();
+
+    if (cd->curlm == NULL) {
+        g_set_error(&fetch_data->error, RESTRAINT_FETCH_LIBARCHIVE_ERROR, 0,
+                "failed to init curl");
+        g_idle_add (archive_finish_callback, fetch_data);
+        return;
+    }
+
+    fetch_data->private_data = cd;
+
+    curl_multi_setopt(cd->curlm, CURLMOPT_SOCKETFUNCTION, sock_cb);
+    curl_multi_setopt(cd->curlm, CURLMOPT_SOCKETDATA, fetch_data);
+    curl_multi_setopt(cd->curlm, CURLMOPT_TIMERFUNCTION, update_timeout_cb);
+    curl_multi_setopt(cd->curlm, CURLMOPT_TIMERDATA, fetch_data);
+
     gboolean open_succeeded = myopen(fetch_data, &tmp_error);
     if (!open_succeeded) {
         g_propagate_error(&fetch_data->error, tmp_error);
         g_idle_add (archive_finish_callback, fetch_data);
         return;
     }
-    r = archive_read_open(fetch_data->a, fetch_data, NULL, myread, myclose);
-    if (r != ARCHIVE_OK) {
-        g_set_error(&fetch_data->error, RESTRAINT_FETCH_LIBARCHIVE_ERROR, r,
-                "archive_read_open failed: %s", archive_error_string(fetch_data->a));
-        g_idle_add (archive_finish_callback, fetch_data);
-        return;
-    }
-    g_idle_add (http_archive_read_callback, fetch_data);
+
+    g_timeout_add(500, start_unpack, fetch_data);
 }
