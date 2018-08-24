c7d59b544b37ed514038beb7a9db6e0961f087bb
Dan Callaghan <dcallagh@redhat.com>
2018-08-24 13:12:38 +1000

    fix to work with recent tito
    Tito seems to have rearranged how/when it creates it build output
    directories, this gets the `tito release` process working again.
    
    Change-Id: I19afcba6b364d47a1a8ad2a17d7fafdd58dc3af4

--- rel-eng/lib/restrainttito.py
+++ rel-eng/lib/restrainttito.py
@@ -10,8 +10,10 @@ class RestraintBuilder(Builder):
         print('Fetching third-party tarballs')
         run_command('make -C third-party tarballs')
         debug('Copying third-party tarballs')
+        self._create_build_dirs()
         for line in open('third-party/tarballs'):
             tarball = line.strip()
+            assert os.path.isdir(self.rpmbuild_sourcedir)
             shutil.copy(os.path.join('third-party', tarball), self.rpmbuild_sourcedir)
-            self.sources.append(tarball)
+            self.sources.append(os.path.join(self.rpmbuild_sourcedir, tarball))
         return super(RestraintBuilder, self).tgz()
