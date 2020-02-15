d39230f669e5fcb6f25554d75270aa17c136ffff
Daniel Rodríguez <danrodri@redhat.com>
2020-02-15 16:05:47 +0100

    Tests: Refactor exit signal handling
    Exit handler is tied to daemon killing, let's change it to add other
    actions.
    
    Move handling to cleanup function and reuse the kill daemon logic.
    Replace pid file names by variables for consistency.
    
    Change-Id: I6d3e9c93a6e3a1640dd2254d02b697875c2be295

--- src/run-tests.sh
+++ src/run-tests.sh
@@ -8,27 +8,39 @@ export MALLOC_CHECK_=2
 export G_DEBUG="fatal_warnings fatal_criticals"
 export G_SLICE="debug-blocks"
 
+GITD_PID_FILE=git-daemon.pid
+HTTPD_PID_FILE=http-daemon.pid
+
 testargs=""
 
-function kill_daemons() {
-    test -f git-daemon.pid && kill -TERM $(cat git-daemon.pid) || :
-    test -f git-daemon.pid && rm -f git-daemon.pid || :
-    test -f http-daemon.pid && kill -TERM $(cat http-daemon.pid) || :
-    test -f http-daemon.pid && rm -f http-daemon.pid || :
+kill_daemon()
+{
+    pid_file=$1
+
+    if [ -f "${pid_file}" ] ; then
+        kill -TERM "$(cat "${pid_file}")" || :
+        rm -f "${pid_file}" || :
+    fi
 }
 
-trap kill_daemons EXIT
+cleanup()
+{
+    kill_daemon ${GITD_PID_FILE}
+    kill_daemon ${HTTPD_PID_FILE}
+}
+
+trap cleanup EXIT
 
 if [[ -f /usr/libexec/git-core/git-daemon ]] ; then
     git daemon --reuseaddr --listen=127.0.0.1 \
         --base-path=test-data/git-remote --export-all --enable=upload-archive \
-        --verbose --detach --pid-file=git-daemon.pid
+        --verbose --detach --pid-file=${GITD_PID_FILE}
 else
     testargs="-s /fetch_git/success -s /fetch_git/fail $testargs"
 fi
 
 function start_httpd() {
-    pidfile=$PWD/http-daemon.pid
+    pidfile=$PWD/${HTTPD_PID_FILE}
     httpd=$PWD/httpserver.py
     (cd test-data/http-remote && python2.7 ${httpd} -i ${pidfile} -p 8000 --host 127.0.0.1 &> /dev/null)
 }
