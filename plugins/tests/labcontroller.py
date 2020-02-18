2c16e971757c5a91784cc57b729b03d64b5e98bc
Martin Styk <mastyk@redhat.com>
2020-02-18 17:24:29 +0100

    chore: clean up mock labcontroller code
    - removed unnecessary imports
    - corrected style
    - updated documentation
    
    Change-Id: Ib0dd392915357233b3da93553fa97149d8efa93b
    Signed-off-by: Martin Styk <mastyk@redhat.com>

--- plugins/tests/labcontroller.py
+++ plugins/tests/labcontroller.py
@@ -1,17 +1,15 @@
-from flask import make_response
-from flask import Flask
-from flask import Response
-from flask import stream_with_context
-import requests
-from flask import request
-import sys
 import logging
+
+from flask import Flask
+from flask import make_response
+from flask import request
 
 logging.basicConfig(level=logging.DEBUG,
                     format='\n[ HTTP Server ] %(message)s)\n'
                     )
 
 app = Flask(__name__)
+
 
 @app.route('/recipes/<recipes_id>/tasks/<task_id>/results/<result_id>/logs/<logfile>', methods=['PUT'])
 def put_data(recipes_id, task_id, result_id, logfile):
@@ -20,6 +18,7 @@ def put_data(recipes_id, task_id, result_id, logfile):
         f.write(request.data.decode('utf-8'))
 
     return 'OK'
+
 
 # For when we dont know the result yet. AKA dmesg upload
 @app.route('/recipes/<recipes_id>/tasks/<task_id>/logs/<logfile>', methods=['PUT'])
@@ -40,7 +39,11 @@ def post_data(recipes_id, task_id):
 
     return response
 
+
 if __name__ == '__main__':
-    """HTTP server used to fake a beaker labcontroller interaction
-       More info here https://beaker-project.org/docs/server-api/http.html#recipe-tasks"""
+    """
+    HTTP server used to fake a Beaker Lab controller interaction.
+    You can find more information in the Beaker Alternative Harness Guide.
+    https://beaker-project.org/docs/alternative-harnesses/
+    """
     app.run(port=8000)
