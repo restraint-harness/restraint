737bb6db577a2d8cce69f72b1a994a39691ee282
Daniel Rodríguez <danrodri@redhat.com>
2020-04-08 18:54:12 +0200

    tests: Add procps-ng dep for containers
    The pidof program is required by restraint and it's needed in tests.

--- containers/provision/fedora:latest.sh
+++ containers/provision/fedora:latest.sh
@@ -6,6 +6,7 @@ dnf -y install 'dnf-command(builddep)' \
 	python3-flask \
 	python3-requests \
 	python3-pytest \
-	python3-six
+	python3-six \
+	procps-ng
 
 dnf -y builddep --spec /restraint/specfiles/restraint-upstream.spec
