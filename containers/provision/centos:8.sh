e91c5eeae74a8abcb9dd867b4799d82203d70fdc
Daniel Rodríguez <danrodri@redhat.com>
2020-04-28 02:25:47 +0200

    test: Add CentOS 8 container provision script

---
+++ containers/provision/centos:8.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dnf install -y 'dnf-command(config-manager)' \
+	       'dnf-command(builddep)' \
+	       make \
+	       rpm-build \
+	       valgrind \
+	       git-daemon \
+	       python3-flask \
+	       python3-requests \
+	       python3-pytest \
+	       python3-six
+
+dnf config-manager -y --enable PowerTools
+
+dnf install -y $(rpmspec -D "_without_static ''"  --buildrequires -q /restraint/restraint.spec)
