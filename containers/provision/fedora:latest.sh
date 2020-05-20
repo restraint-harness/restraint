ebca7094ffa7791f129cdce6b28f08a5b27c28b8
Daniel Rodríguez <danrodri@redhat.com>
2020-05-20 13:36:23 +0200

    test: Remove procps-ng from Fedora provision
    The procps-ng package is no longer needed by Restraint, as the
    dependency with pgreg/pidof was removed.

--- containers/provision/fedora:latest.sh
+++ containers/provision/fedora:latest.sh
@@ -6,7 +6,6 @@ dnf -y install 'dnf-command(builddep)' \
 	python3-flask \
 	python3-requests \
 	python3-pytest \
-	python3-six \
-	procps-ng
+	python3-six
 
 dnf -y builddep --spec /restraint/specfiles/restraint-upstream.spec
