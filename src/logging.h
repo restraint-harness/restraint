cd0fe4b70bddc1cb26bcdfeb052889f71cee277a
Daniel Rodríguez <danrodri@redhat.com>
2020-07-13 12:10:15 +0200

    chore: Replace pragma once with include guard

--- src/logging.h
+++ src/logging.h
@@ -1,4 +1,22 @@
-#pragma once
+/*
+  This file is part of Restraint.
+
+  Restraint is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Restraint is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Restraint.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _RESTRAINT_LOG_MANAGER_H
+#define _RESTRAINT_LOG_MANAGER_H
 
 #include <gio/gio.h>
 #include <libsoup/soup.h>
@@ -35,3 +53,5 @@ void              rstrnt_log                      (const RstrntTask    *task,
                                                    ...) G_GNUC_PRINTF (3, 4);
 
 RstrntLogManager *rstrnt_log_manager_get_instance (void);
+
+#endif
